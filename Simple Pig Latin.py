fd0fb05bdf029b128090b5117f3fe61e11be7fcb
Sharif Orzikulov <orzikulov@gmail.com>
2022-04-03 12:38:01 +0300

    Update Simple Pig Latin.py

--- Simple Pig Latin.py
+++ Simple Pig Latin.py
@@ -1,8 +1,10 @@
-'Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+'''
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
-'Examples
-'pig_it('Pig latin is cool') # igPay atinlay siay oolcay
-'pig_it('Hello world !')     # elloHay orldway !
+Examples
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway ! 
+'''
 
 def pig_it(text):
     splitted_text = text.split(' ')
